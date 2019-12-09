67c583ccd7bee52f5d1966c303f96153343c15b5
Arrumado <>
2019-12-09 20:12:32 -0300

    Front (praticamente finalizado)

--- src/main/java/com/github/braully/dws/VotacaoMB.java
+++ src/main/java/com/github/braully/dws/VotacaoMB.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VotacaoMB {
 
-    @RequestMapping
+    @RequestMapping(value = "/votar")
     public void votar(Integer id) {
         Usuario usuario = null;
         NecessidadePorBairro necessidadePorBairro = null;
